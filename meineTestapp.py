9ce7e69c3b451cfa9a77c8efb02e67b1b72bd6db
kretzschauer <jan.geschinsky@gmx.de>
2023-11-26 11:19:16 +0100

    Update meineTestapp.py

--- meineTestapp.py
+++ meineTestapp.py
@@ -3,6 +3,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from statsmodels import api as sm
 
-csv_file="affen1Trainingsdaten.csv"
+csv_file="affen1trainingsdaten.csv"
 df = pd.read_csv(csv_file)
 st.write("Affentest")
