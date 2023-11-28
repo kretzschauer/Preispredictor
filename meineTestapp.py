b459a54dc218c1c0f423a8cd79a0f50f3c5b8e0e
kretzschauer <jan.geschinsky@gmx.de>
2023-11-28 21:48:05 +0100

    Update meineTestapp.py

--- meineTestapp.py
+++ meineTestapp.py
@@ -4,9 +4,11 @@ from sklearn.model_selection import train_test_split
 from statsmodels import api as sm
 
 st.title('Affentest')
-st.header('Affentest')
+#st.header('Affentest')
+st.markdown('[hinweise](https://cheat-sheet.streamlit.app/)')
 csv_file="affen1trainingsdaten.csv"
 df = pd.read_csv(csv_file,delimiter=";")
 st.write("Affentest")
 #st.dataframe()
 st.table(df.iloc[0:10])
+
